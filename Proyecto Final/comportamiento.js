0e9ac01e53c24b53da10d7948e232b12e274c050
Gonzalomp6 <123137666+Gonzalomp6@users.noreply.github.com>
2023-02-28 22:40:03 -0300

    Add files via upload

---
+++ Proyecto Final/comportamiento.js
@@ -0,0 +1,38 @@
+//var filename = window.location.pathname;
+var filename = '/about.html';
+var link = filename.substring(1, (filename.length-5));
+let navlink = document.querySelector('[href^='+link+']')
+navlink.classList.add('active')
+
+ /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
